ef88653abacc078728175d6591710098c56fd947
qq79402005@gmail.com <qq79402005@gmail.com>
2017-07-26 00:17:36 +0800

    射箭

--- client/game_single/game_single.gd
+++ client/game_single/game_single.gd
@@ -1,25 +1,48 @@
 extends Node2D
 
+var is_aim    = false
+var aim_degree = 0.0
+var aim_adjust_dir = 1.0
+var is_shoot = false
+var shoot_time = 0.0
+export(float) var init_speed = 300
+export(float)   var gravity = 100
+
 func _ready():
-	set_process_input(true)
+	set_process(true)
 	set_fixed_process(true)
 	
-func _input(event):
+func _process(delta):
 	if Input.is_action_pressed("touch"):
 		var weapon = get_node("arrow")
-		weapon.set_gravity_scale(1.0)
-		weapon.set_linear_velocity(Vector2(300, -300))
-
+		aim_degree += delta * 60.0 * aim_adjust_dir
+		if aim_degree > 89.0:
+			aim_adjust_dir = -1.0
+		if aim_degree <0.0:
+			aim_adjust_dir = 1.0
+			
+		weapon.set_rot(deg2rad(aim_degree + 90))
+		
+		is_aim = true
+		
+	if (not Input.is_action_pressed("touch")) and is_aim:
+		is_shoot = true
 
 func _fixed_process(delta):
 	var weapon = get_node("arrow")
-	var velocity = weapon.get_linear_velocity()
-	if velocity.length_squared() > 0.0:
-		velocity = velocity.normalized()
-		var angle = velocity.angle_to(Vector2(-1.0, 0.0))
-		weapon.set_rot( angle)
+	if is_shoot and not weapon.is_colliding():
+		shoot_time += delta
 		
-	#if weapon.get_colliding_bodies().size() > 0 :
-	#	weapon.set_linear_velocity(Vector2(0, 0))
-	#	weapon.set_gravity_scale(0.0)
-	#	print("dfdfdfd")
+		var init_dir = Vector2(0,1).rotated(deg2rad(aim_degree + 90))
+		var init_velocity = init_speed * init_dir#.rotated()
+		var move_velocity = init_velocity
+		move_velocity.y = init_velocity.y + gravity * shoot_time
+		
+		weapon.move( delta * move_velocity)
+		var velocity = move_velocity
+		velocity = velocity.normalized()
+		var angle = Vector2(0.0, 1.0).angle_to(velocity)
+		weapon.set_rot(angle)
+		
+	if weapon.is_colliding():
+		print("diaodiaodiao")
