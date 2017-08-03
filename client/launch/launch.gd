f530ab756c6704b4d24f9800f83ca8b0d40ea0c9
qq79402005@gmail.com <qq79402005@gmail.com>
2017-08-04 01:26:23 +0800

    ad

--- client/launch/launch.gd
+++ client/launch/launch.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 func _ready():
+	if(Globals.has_singleton("Gomob")):
+		print("Gomob")
+		var gomob = Globals.get_singleton("Gomob")
+		gomob.init("your admob Id")
+		gomob.set_test(false)
+		gomob.show()
+	
 	pass
 
 func _on_play_pressed():
