aa01b2ce199004515e6aed8162638ecf24db31ee
Albert Cameron <qq79402005@gmail.com>
2017-08-07 19:47:46 +0800

    wechat support

--- client/global/wechat.gd
+++ client/global/wechat.gd
@@ -6,8 +6,14 @@ func _ready():
 	if(Globals.has_singleton("WeChat")):
 		wechat = Globals.get_singleton("WeChat")
 		wechat.init("wx0123213")
+		wechat.connect("on_resp", self, "on_resp", [])
 		
 func send_msg():
 	if wechat!=null:
 		wechat.send_msg()
 		print("share msg")
+		
+func on_resp(type, errCode, errStr):
+	print(type)
+	print(errCode)
+	print(errStr)
