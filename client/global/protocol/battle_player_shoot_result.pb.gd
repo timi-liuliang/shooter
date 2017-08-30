1c04c98073b93a5123fa49582773394569309132
Albert Cameron <qq79402005@gmail.com>
2017-08-30 18:27:34 +0800

    107 战斗结果由shooter方决定。

--- client/global/protocol/battle_player_shoot_result.pb.gd
+++ client/global/protocol/battle_player_shoot_result.pb.gd
@@ -1,6 +1,7 @@
 extends Node
 
-var player = -1
+var player1_blood = int(0)
+var player0_blood = int(0)
 
 func _ready():
 	pass
@@ -18,11 +19,13 @@ func send(stream):
 	var buf = ByteBuf.new()
 	buf.write_i32(int(id()))
 	buf.write_i32(int(length()))
-	buf.write_i64(player)
+	buf.write_i32(player1_blood)
+	buf.write_i32(player0_blood)
 	buf.write_byte(64)
 	buf.write_byte(64)
 	stream.put_data(buf.raw_data())
 
 func parse_data( byteBuffer):
-	player = byteBuffer.read_i64();
+	player1_blood = byteBuffer.read_i32();
+	player0_blood = byteBuffer.read_i32();
 	pass
