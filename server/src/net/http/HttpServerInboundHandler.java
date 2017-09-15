d04f3386b1e88e67813e2049bc2c88a29f9b01f5
Albert Cameron <qq79402005@gmail.com>
2017-09-15 11:13:11 +0800

    使用http服务查询服务器状态。

--- server/src/net/http/HttpServerInboundHandler.java
+++ server/src/net/http/HttpServerInboundHandler.java
@@ -1,13 +1,24 @@
 package net.http;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.DefaultFullHttpRequest;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.DefaultHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.QueryStringDecoder;
+import io.netty.util.CharsetUtil;
+import manager.player.Player;
+import manager.room.RoomMgr;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -33,7 +44,19 @@ public class HttpServerInboundHandler extends SimpleChannelInboundHandler<FullHt
 	
 	private void handleHttpGet(ChannelHandlerContext ctx, FullHttpRequest request) {
 		final String uri = request.getUri();
-		logger.info(uri);
+		logger.info("http request[" + uri + "]");
+		
+		if(uri.equals("/state")) {
+			String sendMsg = String.format("Rooms number [%d]\nPlayers number[%d]", RoomMgr.rooms.size(), Player.players.size());
+			ByteBuf content = Unpooled.copiedBuffer(sendMsg, CharsetUtil.UTF_8);
+			
+			DefaultFullHttpResponse msg = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+			msg.headers().set(HttpHeaders.Names.CONTENT_TYPE, "application/json; charset=utf-8");
+			msg.headers().set(HttpHeaders.Names.CONTENT_LENGTH, msg.content().readableBytes());
+			ctx.write(msg).addListener(ChannelFutureListener.CLOSE);
+			ctx.flush();
+		}
+		
 	}
 	
 	private void handleHttpPost(ChannelHandlerContext ctx, FullHttpRequest request) {
