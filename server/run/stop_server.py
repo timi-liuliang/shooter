9dee42c08ef71b9dc47cd00b1068360fe2b1f45a
Albert <qq79402005@gmail.com>
2017-09-18 20:47:11 -0700

    a

--- server/run/stop_server.py
+++ server/run/stop_server.py
@@ -1,3 +1,9 @@
 import os
+import psutil
 
-os.system("(java -jar server.jar &)")
+cmdline_name = ['java', '-jar', 'server.jar']
+
+for proc in psutil.process_iter():
+    if proc.cmdline() == cmdline_name:
+        print('stop server [kill -15 %d]' % proc.pid)
+        os.system('kill -15 %d' % proc.pid)
